f4bfeb2c6eb863cdb89ac58df729557d29be7fbd
Shashikala-11 <shashikalagupta.rajesh.com>
2026-06-11 01:13:06 +0530

    most busy user

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 import preprocessor,helper
-# from helper import fetch_stats
+import matplotlib.pyplot as plt
 
 st.sidebar.title('Whatsapp Chat Analyzer')
 
@@ -29,12 +29,45 @@ if uploaded_file is not None:
 
         # passing the name of selected user to fetch_stats function to get the total no. of messages.
 
-        num_messages=helper.fetch_stats(selected_user,df)
+        num_messages,words,num_media_messages,num_links=helper.fetch_stats(selected_user,df)
         col1,col2,col3,col4=st.columns(4)
 
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Total Media Count")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Total Links Shared")
+            st.title(num_links)
+
+        # finding the busiest/ chatiest users in the group (Group level)
+        st.title("Most Busy Users")
+        if selected_user=='Overall':
+           
+            x,new_df=helper.fetch_most_busy_users(df)
+            fig,ax=plt.subplots()
+            
+            
+
+            c1,c2=st.columns(2)
+            with c1:
+                ax.bar(x.index,x.values,color='orange')
+                plt.xticks(rotation=-270)
+                st.pyplot(fig)
+
+            with c2:
+                st.dataframe(new_df)
+                pass    
+                
+
+
+
+                    
         pass
 
 
--- helper.py
+++ helper.py
@@ -1,11 +1,37 @@
+from urlextract import URLExtract
+
+
 def fetch_stats(selected_user,df):
-    if selected_user=='Overall':
-        # 1. fetch number of messages
-        num_messages=df.shape[0]
-        # 2. number of words
-        words=[]
-        for message in df['message']:
+    if selected_user!='Overall':
+        df=df[df['user']==selected_user]
+    # 1. fetch number of messages
+    num_messages=df.shape[0]
+    # 2. number of words
+    words=[]
+    for message in df['message']:
             words.extend(message.split())
-        return num_messages,len(words)    
-    else:
-        return df[df['user']==selected_user].shape[0]
+    # 3. fetch number of media messages
+    num_media_messages=df['message'].str.contains('<Media omitted>\n',na=False).sum()
+    # 4. fetch number of links across messages shared
+    links=[]
+
+    extractor=URLExtract()
+    for message in df['message']:
+         links.extend(extractor.find_urls(message))
+    num_links=len(links)
+
+
+
+
+    return num_messages,len(words),num_media_messages,num_links  
+
+def fetch_most_busy_users(df):
+     
+    x=df['user'].value_counts().head()
+    df=round((df['user'].value_counts()/df.shape[0])*100,2).reset_index().rename(columns={'user':'Name','count':'Percent'})
+
+    return x,df
+
+
+
+    
